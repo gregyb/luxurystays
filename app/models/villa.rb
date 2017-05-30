2f2b15749e9648481b1c3a984b746ba8b48605d1
Axel Roffi <axel.roffi@gmail.com>
2017-05-30 17:31:59 +0200

    Adding villascontroller actions & coding views

--- app/models/villa.rb
+++ app/models/villa.rb
@@ -1,9 +1,9 @@
 class Villa < ApplicationRecord
 
-  validates: :title, presence: true
-  validates: description:, presence: true
-  validates: :city, presence: true
-  validates: :price, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :city, presence: true
+  validates :price, presence: true
 
 
 end
