e4f062a1485de50e2b4620540ae3b48bb410d0db
Grégoire Basset <gregoirebasset@gmail.com>
2017-05-31 11:25:59 +0200

    Villa: Added User Reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530092149) do
+ActiveRecord::Schema.define(version: 20170531090800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20170530092149) do
     t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_villas_on_user_id"
   end
 
 end
